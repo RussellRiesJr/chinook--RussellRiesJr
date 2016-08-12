2c0d421e48ff56f5f1cd9c8681f95c31e8e2c385
Russell Ries Jr <riesru@gmail.com>
2016-08-12 14:36:53 -0500

    add top_2009_agent solution

---
+++ top_2009_agent.sql
@@ -0,0 +1,15 @@
+
+select
+    SalesReview.FirstName,
+    SalesReview.LastName,
+    max(SalesReview.TotalSales) as Gross
+from
+    (select
+        e.FirstName as FirstName,
+        e.LastName as LastName,
+        sum(i.Total) as 'TotalSales'
+    from Employee e, Customer c, Invoice i
+    where e.EmployeeId = c.SupportRepId
+    and c.CustomerId = i.CustomerId
+    and i.invoiceDate like '2009%'
+    group by e.LastName) as SalesReview
